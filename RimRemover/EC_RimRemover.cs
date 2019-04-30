e76f0b64e288f03e31b5c24b172d7d8ba43741a5
Kokaiinum <42316813+Kokaiinum@users.noreply.github.com>
2019-04-30 04:29:02 +0100

    Add files via upload

---
+++ RimRemover/EC_RimRemover.cs
@@ -0,0 +1,40 @@
+﻿using Harmony;
+using BepInEx;
+using System;
+using UnityEngine;
+
+namespace EC_RimRemover
+{
+    //Original Koikatsu plugin by https://github.com/geneishouko
+    [BepInProcess("EmotionCreators")]
+    [BepInPlugin(GUID: "EC.RimRemover", Name: "Rim Remover", Version: "1.0")]
+    public class RimRemover : BaseUnityPlugin
+    {
+        void Awake()
+        {
+
+            var harmony = HarmonyInstance.Create("Rim Remover");            
+            BepInEx.Harmony.HarmonyWrapper.PatchAll(typeof(RimRemover));
+          
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ChaControl),"LoadCharaFbxDataAsync")]
+        public static void RemoveRim(ChaControl __instance, ref Action<GameObject> actObj)
+        {
+            Action<GameObject> oldAct = actObj;
+            actObj = delegate (GameObject o)
+            {
+                oldAct(o);
+                if (o == null)
+                    return;
+                var renderers = o.GetComponentsInChildren<Renderer>();
+                foreach (var r in renderers)
+                    r.material.SetFloat("_rimV", 0f);
+                //oldAct(o);
+            };
+        }
+
+   }
+
+}
