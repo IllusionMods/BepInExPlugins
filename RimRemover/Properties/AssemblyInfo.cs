50cf0b75a7467f24b565c774d1994a951cf08fc7
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-10-22 14:30:38 -0500

    Update AssemblyInfo

--- RimRemover/Properties/AssemblyInfo.cs
+++ RimRemover/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KoikatsuUnlimited")]
+[assembly: AssemblyTitle("RimRemover")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KoikatsuUnlimited")]
+[assembly: AssemblyCompany("https://github.com/IllusionMods/RimRemover")]
+[assembly: AssemblyProduct("RimRemover")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
