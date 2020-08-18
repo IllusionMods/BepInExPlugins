8a17acb07b2eec9c7b8f8dbc00d8693db54fffdd
ManlyMarco <dummyaccx@gmail.com>
2020-08-18 02:07:50 +0200

    Use nuget for deps; Update to bepin 5.1

--- RimRemover/EC_RimRemover.cs
+++ RimRemover/EC_RimRemover.cs
@@ -1,40 +1,35 @@
-﻿using Harmony;
-using BepInEx;
+﻿using BepInEx;
 using System;
+using HarmonyLib;
 using UnityEngine;
 
 namespace EC_RimRemover
 {
-    //Original Koikatsu plugin by https://github.com/geneishouko
     [BepInProcess("EmotionCreators")]
-    [BepInPlugin(GUID: "EC.RimRemover", Name: "Rim Remover", Version: "1.0")]
+    [BepInPlugin("EC.RimRemover", "Rim Remover", "1.0")]
     public class RimRemover : BaseUnityPlugin
     {
-        void Awake()
-        {
+        private static readonly int _rimV = Shader.PropertyToID("_rimV");
 
-            var harmony = HarmonyInstance.Create("Rim Remover");            
-            BepInEx.Harmony.HarmonyWrapper.PatchAll(typeof(RimRemover));
-          
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(RimRemover));
         }
 
         [HarmonyPrefix]
-        [HarmonyPatch(typeof(ChaControl),"LoadCharaFbxDataAsync")]
+        [HarmonyPatch(typeof(ChaControl), "LoadCharaFbxDataAsync")]
         public static void RemoveRim(ChaControl __instance, ref Action<GameObject> actObj)
         {
-            Action<GameObject> oldAct = actObj;
+            var oldAct = actObj;
             actObj = delegate (GameObject o)
             {
                 oldAct(o);
-                if (o == null)
-                    return;
+                if (o == null) return;
                 var renderers = o.GetComponentsInChildren<Renderer>();
                 foreach (var r in renderers)
-                    r.material.SetFloat("_rimV", 0f);
+                    r.material.SetFloat(_rimV, 0f);
                 //oldAct(o);
             };
         }
-
-   }
-
+    }
 }
